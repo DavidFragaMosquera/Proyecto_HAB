7a3987592d3569b116dfd1366c401e499fc91a71
DavidFragaMosquera <davidrosales1990@gmail.com>
2020-04-27 13:10:02 +0200

    arreglos BBDD

--- SQL/activosFotograficos.sql
+++ SQL/activosFotograficos.sql
@@ -1,76 +1,121 @@
-CREATE DATABASE activos_fotograficos;
-USE activos_fotograficos
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE usuarios (
-  id_usuarios INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
-  login VARCHAR(33) NOT NULL,
-  password VARCHAR(33) NOT NULL,
-  nombre VARCHAR(33) NOT NULL,
-  apellidos VARCHAR(33),
-  fecha_nacimiento DATE NOT NULL,
-  dni VARCHAR(9) NOT NULL,
-  direccion VARCHAR(133) NOT NULL,
-  mail VARCHAR(100) NOT NULL,
-  telefono VARCHAR(8),
-  imagenes VARCHAR  (255),
-  descripcion VARCHAR (1000),
-  primer_acceso DATETIME NOT NULL,
-  ultimo_acceso DATETIME NOT NULL,
-  ip_ultimo_acceso INT NOT NULL
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE TABLE articulos (
-  id_articulos INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
-  id_usuarios INT UNIQUE NOT NULL,
-  imagenes VARCHAR (255),
-  tipo enum ('cesion de imagenes', 'alquiler de equipos'),
-  subtipo enum ('e-commerce y producto','bodas y eventos', 'retrato y animales', 'camaras reflex', 'drones', 'accesorios'),
-  CONSTRAINT FK_usuarios foreign key(id_usuarios) references usuarios (id_usuarios)
-);
+-- -----------------------------------------------------
+-- Schema ActivosFotograficos
+-- -----------------------------------------------------
 
-CREATE TABLE productos (
-  id_productos INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
-  id_articulos_id INT UNIQUE NOT NULL,
-  id_usuarios INT UNIQUE NOT NULL,
-  precio VARCHAR(100),
-  descripcion VARCHAR(1000),
-  fecha_inicio DATE,
-  fecha _fin DATE,
-  Fecha_ultimo_acceso DATETIME,
-  CONSTRAINT FK_usuarios foreign key(id_usuarios) references usuarios (id_usuarios),
-  CONSTRAINT FK_articulos foreign key(id_articulos) references articulos (id_articulos)  
-);
+-- -----------------------------------------------------
+-- Schema ActivosFotograficos
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `ActivosFotograficos` DEFAULT CHARACTER SET utf8 ;
+USE `ActivosFotograficos` ;
 
-CREATE TABLE pedidos (
-  id_pedidos INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
-  id_productos VARCHAR UNIQUE NOT NULL,
-  id_usuarios INT  UNIQUE NOT NULL,
-  Dirección VARCHAR (150) NOT NULL,
-  fecha_envio DATE,
-  fecha_inicio DATETIME,
-  fecha_fin DATETIME,
-  precio VARCHAR(100),
-  CONSTRAINT FK_usuarios foreign key(id_usuarios) references usuarios (id_usuarios),
-  CONSTRAINT FK_productos foreign key(id_productos) references productos (id_productos)
-);
-
-CREATE TABLE rating (
-  id_rating INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
-  producto_ID INT UNIQUE,
-  rate INT CHECK (rate between 1 and 5)
-  comentario VARCHAR(1000),
-  CONSTRAINT FK_productos foreign key(id_productos) references productos (id_productos)
-);
+-- -----------------------------------------------------
+-- Table `ActivosFotograficos`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ActivosFotograficos`.`usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `login` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `nombre` VARCHAR(20) NOT NULL,
+  `apellidos` VARCHAR(50) NULL,
+  `fecha_nacimiento` DATE NOT NULL,
+  `dni` VARCHAR(45) NOT NULL,
+  `direccion` VARCHAR(100) NOT NULL,
+  `telefono` VARCHAR(15) NULL,
+  `mail` VARCHAR(45) NOT NULL,
+  `imagen` VARCHAR(45) NULL,
+  `descripcion` VARCHAR(1000) NULL,
+  `primer_acceso` DATETIME NOT NULL,
+  `ultimo_acceso` DATETIME NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `ActivosFotograficos`.`articulo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ActivosFotograficos`.`articulo` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL,
+  `imagen` VARCHAR(50) NULL,
+  `descripcion` VARCHAR(1000) NULL,
+  `precio` VARCHAR(45) NULL,
+  `tipo` ENUM('cesion derechos', 'alquiler') NOT NULL,
+  `subtipo` ENUM('e-commerce y producto', 'bodas y eventos', 'retrato y animales', 'camaras reflex', 'drones', 'accesorios') NOT NULL,
+  `fecha_inicio` DATE NOT NULL,
+  `fecha_fin` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_usuario_idx` (`id` ASC),
+  CONSTRAINT `id`
+    FOREIGN KEY (`id`)
+    REFERENCES `ActivosFotograficos`.`usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+PACK_KEYS = DEFAULT;
 
-CREATE TABLE consultas (
-  id_consultas INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
-  id_articulos INT UNIQUE NOT NULL,
-  id_productos INT UNIQUE NOT NULL,
-  ip INT,
-  CONSTRAINT FK_articulos foreign key(id_articulos) references articulos (id_articulos),
-  CONSTRAINT FK_productos foreign key(id_productos) references productos (id_productos)
-);
+
+-- -----------------------------------------------------
+-- Table `ActivosFotograficos`.`pedido`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ActivosFotograficos`.`pedido` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL,
+  `precio` VARCHAR(45) NULL,
+  `direccion` VARCHAR(45) NULL,
+  `fecha_envio` DATE NULL,
+  `fecha_inicio` DATE NULL,
+  `fecha_fin` DATE NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_articulo_idx` (`id` ASC),
+  CONSTRAINT `id_articulo`
+    FOREIGN KEY (`id`)
+    REFERENCES `ActivosFotograficos`.`articulo` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `ActivosFotograficos`.`rating`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ActivosFotograficos`.`rating` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL,
+  `valoracion` INT NULL,
+  `comentario` VARCHAR(1000) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_articulo_idx` (`id` ASC),
+  CONSTRAINT `id_articulo`
+    FOREIGN KEY (`id`)
+    REFERENCES `ActivosFotograficos`.`articulo` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `ActivosFotograficos`.`consultas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ActivosFotograficos`.`consultas` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL,
+  `ip` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_articulo_idx` (`id` ASC),
+  CONSTRAINT `id_articulo`
+    FOREIGN KEY (`id`)
+    REFERENCES `ActivosFotograficos`.`articulo` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
